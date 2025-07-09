b6b779a9139f9801890dd4f229f9959c38c5ebca
Sameen-K-A <sameensameen60@gmail.com>
2025-07-09 13:32:33 +0530

    Position updated

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -12,7 +12,7 @@ export default function HeroSection() {
     <FadeInOnView duration={3}>
       <section className="min-h-screen w-full flex items-center justify-center px-5 sm:px-8 md:px-12 lg:px-15">
         <div className="absolute w-full z-1 bottom-0 bg-gradient-to-t from-black to-transparent">
-          <div className="flex flex-col items-center pb-10">
+          <div className="flex flex-col items-center pb-28 md:pb-10">
             <span className="mb-2 bg-gradient-to-r from-white to-gray-300/60 bg-clip-text w-fit text-4xl sm:text-5xl md:text-6xl font-bold leading-none text-transparent">You&rsquo;ll share your</span>
             <span className="mb-2 bg-gradient-to-r from-white to-gray-300/60 bg-clip-text w-fit text-4xl sm:text-5xl md:text-6xl font-bold leading-none text-transparent">vision.</span>
             <span className="mb-2 bg-gradient-to-r from-white to-gray-300/60 bg-clip-text w-fit text-4xl sm:text-5xl md:text-6xl font-bold leading-none text-transparent">We&rsquo;ll make it</span>
